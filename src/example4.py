93027d0a7c58477178703fcebcd20f2c0be41330
Maximilian <108086088+ONEMILIAN@users.noreply.github.com>
2023-01-05 18:00:46 +0100

    Added example4.py

---
+++ src/example4.py
@@ -0,0 +1,26 @@
+state = "on"
+zahl1 = ""
+operator = ""
+zahl2 = ""
+_continue = True
+
+def getResult(eingabe1, eingabe2):
+    if eingabeOperator == "*":
+        return eingabe1 * eingabe2
+    elif eingabeOperator == "-":
+        return eingabe1 - eingabe2
+    elif eingabeOperator == "+":
+        return eingabe1 + eingabe2
+    elif eingabeOperator == "/" or "%":
+        return eingabe1 / eingabe2
+
+
+print("Willkommen in der TaschenrechnerApp! Geben sie eine Zahl ein \n")
+while state == "on":
+    eingabeZahl1 = input()
+    print("Nun geben sie einen Operator an. \n")
+    eingabeOperator = input()
+    print("Geben sie als letztes eine zweite Zahl an. \n")
+    eingabeZahl2 = input()
+    print("Ergebnis ist: ", getResult(int(eingabeZahl1), int(eingabeZahl2)))
+
